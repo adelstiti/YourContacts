863d3e8453570af242f493e8a55361aff9563632
Adel Stiti <adelstiti@users.noreply.github.com>
2020-01-14 22:11:49 +0100

    Some Edit

--- server.js
+++ server.js
@@ -18,13 +18,8 @@ app.use('/api/auth',require('./routes/auth'));
 
 // Serve Static Assets if in production
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
 
-    app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    } )
-}
+
 const PORT = process.env.PORT || 5000 ;
 
 app.listen(PORT,() => console.log(`Server Started on port ${PORT} `));
