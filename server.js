cc3d5f937223b90a3b347489923158c6093e6bfa
Adel Stiti <adelw126@gmail.com>
2022-12-31 17:17:32 +0100

    Finish deploy configuration

--- server.js
+++ server.js
@@ -18,20 +18,13 @@ app.use('/api/auth',require('./routes/auth'));
 
 // Serve Static Assets if in production
 
-// if(process.env.NODE_ENV === 'production'){
-    // app.use(express.static('client/build'));
-    app.use(express.static(__dirname, './client/build'));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
 
     app.get('*',(req,res) => {
-        res.sendFile(
-            // path.resolve(__dirname,'client','build','index.html')
-            path.join(__dirname, "./client/build/index.html"),
-            function (err) {
-                res.status(500).send(err);
-            }
-        )
-    })
-// }
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    } )
+}
 
 const PORT = process.env.PORT || 5000 ;
 
